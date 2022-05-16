2b9c41a533eecb097f76889b819efbd07bbb240f
PrajwalCJ <prajwalspet@gmail.com>
2022-05-16 21:06:28 +0530

    deploy

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>Please check the input!</h2>
+    </div>
+</template>
+<style scoped>
+    h2{
+        color: red
+    }
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        v-bind:checked="isDone"
+        @change="$emit('checkbox-changed')"
+      />
+      <label :for="id">{{ label }}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn" @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{ label }}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{ label }}</span>
+      </button>
+    </div>
+  </div>
+  <to-do-item-edit-form
+    v-else
+    :id="id"
+    :label="label"
+    @item-edited="itemEdited"
+    @edit-cancelled="editCancelled"
+  ></to-do-item-edit-form>
+</template>
+<script>
+import ToDoItemEditForm from "./ToDoItemEditForm";
+
+export default {
+  props: {
+    label: {
+      required: true,
+      type: String,
+    },
+    done: {
+      default: false,
+      type: Boolean,
+    },
+    id: {
+      required: true,
+      type: String,
+    },
+  },
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit("item-edited", newLabel);
+      this.isEditing = false;
+    },
+    editCancelled() {
+      this.isEditing = false;
+    },
+  },
+  components: {
+    ToDoItemEditForm,
+  },
+  computed:{
+      isDone(){
+          return this.done;
+      }
+  }
+};
+</script>
